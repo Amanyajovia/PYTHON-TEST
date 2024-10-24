de0fd843f523029865c05c7ea95645aba667c027
Amanyajovia <amanyajovia7@gmail.com>
2024-10-24 11:24:24 +0300

    completed python test

---
+++ question 1.py
@@ -0,0 +1,21 @@
+#Question 1(i)
+# Write a Python program to find the distance between two coordinate points (x1, y1) and (x2, y2).
+coordinates = ('x1,y1') and ('x2,y2')
+import math
+math.sqrt()
+
+Distance = (x2-x1)**2 + (y2-y1)**2
+print(Distance)
+
+
+
+
+
+
+# Question 1(ii)
+# Write a Python program to find maximum between three numbers.
+number_one =int(input("Enter number one: "))
+number_two = int(input("Enter number two: "))
+number_three = int(input("Enter number three: "))
+maximum = (number_one, number_two, number_three)
+print(f"The maximum between {number_one},{number_two} and {number_three}")
---
+++ question3.py
@@ -0,0 +1,14 @@
+# Question 3(i)
+#  Write a Python program that prompts a user to enter numbers. The process will repeat until
+#  the user enters 0. Finally, the program prints sum of the numbers entered by the user.
+
+number_one = int(input("Enter number one: "))
+number_two = int(input("Enter number two: "))
+total_sum = number_one + number_two
+print(total_sum)
+
+
+
+# Question 3(ii)
+# Write a Python program to print all the numbers from 1 to 100 that are not divisible by 2
+
